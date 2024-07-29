44411f32868319abd3f1f1f54ec1ea84b6ef4122
Mikhail Mishin <mishin_mihail_98@mail.ru>
2024-07-29 17:44:48 +0300

    commit2

---
+++ src/main/java/org/example/springboot/service/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package org.example.springboot.service;
+
+import org.example.springboot.model.User;
+import org.example.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id){
+        return userRepository.getOne(id);
+    }
+
+    @Override
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(User user) {
+        User existingUser = userRepository.findById(user.getId()).orElse(null);
+        if (existingUser != null) {
+            existingUser.setName(user.getName());
+            existingUser.setEmail(user.getEmail());
+            return userRepository.save(existingUser);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id){
+        userRepository.deleteById(id);
+    }
+}
