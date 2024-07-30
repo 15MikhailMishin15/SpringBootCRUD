ef8b409617c2978bfa3c529e8279dd63fb53b062
Mikhail Mishin <mishin_mihail_98@mail.ru>
2024-07-31 01:26:37 +0300

    commit 3

--- src/main/java/org/example/springboot/service/UserServiceImpl.java
+++ src/main/java/org/example/springboot/service/UserServiceImpl.java
@@ -35,10 +35,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User updateUser(User user) {
-        User existingUser = userRepository.findById(user.getId()).orElse(null);
-        if (existingUser == null) {
-            throw new EntityNotFoundException("User not found with id: " + user.getId());
-        }
+        User existingUser = userRepository.findById(user.getId())
+                .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + user.getId()));
+
         existingUser.setName(user.getName());
         existingUser.setEmail(user.getEmail());
         return userRepository.save(existingUser);
